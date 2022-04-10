f7db0bb567d81e1eafa6f2e61cdeee69bcbc106a
Anderson0312 <74934630+Anderson0312@users.noreply.github.com>
2022-04-10 15:06:17 -0300

    1

---
+++ Cursos/porjetoPiano/main.js
@@ -0,0 +1,23 @@
+// get all keys
+const keys = document.querySelectorAll('.key')
+
+
+// play notes
+function playNote(event) {
+    //keycods
+
+    //typed or pressed key
+
+    //if key exist
+
+    // play audio
+
+}
+
+
+// click with mouse
+keys.forEach()
+
+
+// keyboar type
+window.addEventListener('keydown', playNote)
